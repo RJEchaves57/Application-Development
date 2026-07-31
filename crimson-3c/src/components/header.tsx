f3528b5526bd16aeff76cb84cfe7e85e9eac2a35
RJ_Echaves <joelroqueechaves@gmail.com>
2026-07-31 22:30:48 +0800

    header.tsx

---
+++ crimson-3c/src/components/header.tsx
@@ -0,0 +1,9 @@
+import { NavLink } from "react-router";
+
+export function Header() {
+  return (
+    <nav className="bg-gray-900">
+      <a href="/" className="text-white px-4 py-2">Home</a>
+    </nav>
+  );
+}
